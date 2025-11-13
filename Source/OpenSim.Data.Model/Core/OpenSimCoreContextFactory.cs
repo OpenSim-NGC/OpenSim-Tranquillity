cb826bc57b54f3c91f3fca819a034fc07dc98fb9
Mike Dickson <mdickson.dickson@utopiaskye.com>
2025-11-12 20:27:50 -0500

    Add new MPL v2 header for  files added to the release.

--- Source/OpenSim.Data.Model/Core/OpenSimCoreContextFactory.cs
+++ Source/OpenSim.Data.Model/Core/OpenSimCoreContextFactory.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿/* Copyright (c) 2025 Utopia Skye LLC
+
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+ */
+
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 
--- Source/OpenSim.Data.Model/Core/Userdatum.cs
+++ Source/OpenSim.Data.Model/Core/Userdatum.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* Copyright (c) 2025 Utopia Skye LLC
+
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+ */
+
+using System;
 using System.Collections.Generic;
 
 namespace OpenSim.Data.Model.Core;
