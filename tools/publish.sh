e6ba9901b19ec3d5b882e3f9729da858fc9b5590
Mike Dickson <mdickson.dickson@utopiaskye.com>
2026-01-16 14:40:30 -0500

    Rather than compile to seperate bin directories per project (which is where we ultimately want to get to) flatten the build and compile to a top level build directory.  The seperate bin folders causes issues with the current plugin stuff.  We can switch back when thats gone and we're using DI for initialization.

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-mkdir -p publish
-
-dotnet publish -o publish/OpenSim.Server.RegionServer Source/OpenSim.Server.RegionServer/OpenSim.Server.RegionServer.csproj
-dotnet publish -o publish/OpenSim.Server.GridtServer Source/OpenSim.Server.GridServer/OpenSim.Server.GridServer.csproj
-dotnet publish -o publish/OpenSim.Server.MoneyServer  Source/OpenSim.Server.MoneyServer/OpenSim.Server.MoneyServer.csproj
+dotnet publish Tranquillity.sln
