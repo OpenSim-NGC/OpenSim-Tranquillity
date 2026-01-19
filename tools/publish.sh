6d422565265e0c2b853c52b7545bf51f362eba5d
Mike Dickson <mike.dickson@utopiaskye.com>
2026-01-19 11:20:26 -0500

    Add templates for startup scripts.  Add the platform to the publish directory to distinquish linux from windows.

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-dotnet publish Tranquillity.sln -c Release -r linux-x64 --self-contained false -o ./publish
-dotnet publish Tranquillity.sln -c Release -r win-x64 --self-contained false -o ./publish
+dotnet publish Tranquillity.sln -c Release -r linux-x64 --self-contained false -o ./publish/linux-x64
+dotnet publish Tranquillity.sln -c Release -r win-x64 --self-contained false -o ./publish/win-x64
