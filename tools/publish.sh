dfe3ae562dc98ba1f72c587ba2387714b14e8094
Mike Dickson <mdickson.dickson@utopiaskye.com>
2026-03-28 17:34:30 -0400

    - Move assets and inventory to OpenSim.Services.InventoryService and copy them into the build output and publish directories on build or publish.  Keeps the data attached to the project that consumes it. - Don't build to a single build output directory. This uses the default obj and bin for each project for build output.  We had cases with the shared directory where dependencies were getting clobbered from multiple projects. - Add multiple publish commands to tools/publish.sh. One for each server.   RegionServer still likely requires some additional dependencies to end up with runable output. - Add some helper scripts to GridServer for startup/shutdown/etc.  They need to be integrated into the Dockerfile. Need to resolve a caching issue when doing a Docker build.  But the basisc of a file for each executable service are in place.

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-dotnet publish Tranquillity.sln -c Release -r linux-x64 --self-contained false -o ./publish/linux-x64
+# dotnet publish Tranquillity.sln -c Release -r linux-x64 --self-contained false -o ./publish/linux-x64
 # dotnet publish Tranquillity.sln -c Release -r win-x64 --self-contained false -o ./publish/win-x64
+dotnet publish "Source/OpenSim.Server.GridServer/OpenSim.Server.GridServer.csproj" -c Release -r linux-x64 -o ./publish/OpenSim.Server.GridServer
+dotnet publish "Source/OpenSim.Server.RegionServer/OpenSim.Server.RegionServer.csproj" -c Release -r linux-x64 -o ./publish/OpenSim.Server.RegionServer
+dotnet publish "Source/OpenSim.Server.MoneyServer/OpenSim.Server.MoneyServer.csproj" -c Release -r linux-x64 -o ./publish/OpenSim.Server.MoneyServer
