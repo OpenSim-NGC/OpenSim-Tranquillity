ea9c427f92aeb3f11c2e5cc4be1e9421da7eb1f8
Mike Dickson <mike.dickson@utopiaskye.com>
2026-08-11 00:04:41 -0400

    Fixup startup handling for grid server and add grid-command.sh

--- Source/OpenSim.Server.GridServer/AppData/Scripts/grid-start.sh
+++ Source/OpenSim.Server.GridServer/AppData/Scripts/grid-start.sh
@@ -6,12 +6,6 @@ BASE_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
 # saner programming env: these switches turn some bugs into errors
 set -o errexit -o pipefail -o noclobber -o nounset
 
-! getopt --test > /dev/null 
-if [[ ${PIPESTATUS[0]} -ne 4 ]]; then
-    echo "I’m sorry, `getopt --test` failed in this environment."
-    exit 1
-fi
-
 # handle non-option arguments
 if [[ $# -ne 1 ]]; then
     echo "$0: A grid service name is required."
