e8452b1422eb52beb8c6f898273af1bddf7c50cc
Mike Dickson <mike.dickson@utopiaskye.com>
2026-08-27 21:32:21 -0400

    Re-add Patches from Consortium added in an MR by FlyMan.

--- Source/OpenSim.Region.CoreModules/Framework/InventoryAccess/InventoryAccessModule.cs
+++ Source/OpenSim.Region.CoreModules/Framework/InventoryAccess/InventoryAccessModule.cs
@@ -905,7 +905,11 @@ public class BasicInventoryAccessModule : INonSharedRegionModule, IInventoryAcce
         InventoryItemBase item = m_Scene.InventoryService.GetItem(remoteClient.AgentId, itemID);
 
         if (item is null)
-            return null;
+        {
+            item = m_Scene.LibraryService.GetItem(itemID);
+            if (item == null)
+                return null;
+        }
 
         if (attachment && (item.Flags & (uint)InventoryItemFlags.ObjectHasMultipleItems) != 0)
         {
